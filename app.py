aa40e977b1a7e66d0197c4a21bf6afbd8b48917c
Juan Pablo Blandon B <166535711+juanpBB@users.noreply.github.com>
2025-03-19 15:48:52 -0500

    guardamos la ruta de la aplicacion

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ import database as dbase
 from product import Product
 
 db = dbase.dbConnection()
+
+app = Flask(__name__)
+
+#Rutas de la aplicación
+@app.route('/')
+def home():
+    products = db['products']
+    productsReceived = products.find()
+    return render_template('index.html', products = productsReceived)
